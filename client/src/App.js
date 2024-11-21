38086d77ee38485564ef54abe1e07298e5cec8d8
Abhishek Soni <134426130+abhisoni24@users.noreply.github.com>
2024-11-21 16:06:12 -0600

    restructure project again

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>ClinicsNepal</h1>
-    </div>
-  );
-}
-
-export default App;
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const port = process.env.PORT || 5000;
-
-console.log(process.env.MONGO_URL);
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
