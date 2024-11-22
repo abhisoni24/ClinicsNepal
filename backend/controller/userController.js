bf255a4b6ad1c1804ae99cc124416af879783ec3
Abhishek Soni <134426130+abhisoni24@users.noreply.github.com>
2024-11-22 01:18:05 -0600

    add login to backend

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,6 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
-import User from "../models/userSchema.js";
+import { User } from "../models/userSchema.js";
 
 export const patientRegister = catchAsyncErrors(async (req, res, next) => {
   const {
@@ -47,5 +47,33 @@ export const patientRegister = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "User registered successfully",
+  });
+});
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password, confirmPassword, role } = req.body;
+  if (!email || !password || !confirmPassword || !role) {
+    return next(new ErrorHandler("Please fill in all fields", 400));
+  }
+  if (password !== confirmPassword) {
+    return next(
+      new ErrorHandler("Password and confirm password do not match", 400)
+    );
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 400));
+  }
+
+  if (role !== user.role) {
+    return next(new ErrorHandler("User with this role not found", 400));
+  }
+  res.status(200).json({
+    success: true,
+    message: "User logged in successfully",
   });
 });
